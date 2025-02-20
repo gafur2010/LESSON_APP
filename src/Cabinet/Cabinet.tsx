2ba9c018cafe65c2fd338cab05668d5fa236f1f7
G'afur <gafur2224533@gmail.com>
2025-02-20 14:57:13 +0500

    third commit

--- src/Cabinet/Cabinet.tsx
+++ src/Cabinet/Cabinet.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { apiCall } from "../apiCall";
 import Posts from "./Posts";
 
--- src/Cabinet/Posts.tsx
+++ src/Cabinet/Posts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { apiCall } from "../apiCall";
 
 interface Post {
